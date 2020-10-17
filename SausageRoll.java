800301260051e73298c16e2bc80b2b91923ef195
TongxinZHU <55003360+TongxinZHU@users.noreply.github.com>
2020-10-16 18:09:33 -0600

    Update SausageRoll.java

--- SausageRoll.java
+++ SausageRoll.java
@@ -1,12 +1,10 @@
 public class SausageRoll extends Roll {
-
     SausageRoll() {
-
+        //To set price of sausage roll, filling, sauce, and topping.
         super("Sausage Roll", 8.50);
         this.fillingPrice = 2.00;
         this.saucePrice = 3.00;
         this.toppingPrice = 4.00;
         this.sauceNum = 1;
     }
-
 }
