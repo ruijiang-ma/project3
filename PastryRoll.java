b74f5923c8e88ca6d91de14456f9b359a12f3594
TongxinZHU <55003360+TongxinZHU@users.noreply.github.com>
2020-10-16 18:05:31 -0600

    Update PastryRoll.java

--- PastryRoll.java
+++ PastryRoll.java
@@ -1,10 +1,10 @@
 public class PastryRoll extends Roll {
     PastryRoll()
     {
+        //To set price of pastry roll, filling, sauce, and topping.
         super("Pastry Roll", 9.00);
         this.fillingPrice = 1.25;
         this.saucePrice = 2.00;
         this.toppingPrice = 1.50;
     }
-
 }
