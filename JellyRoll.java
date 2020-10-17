42b42bf250fcfc9bd1af0a819750a2dc43a997f3
TongxinZHU <55003360+TongxinZHU@users.noreply.github.com>
2020-10-16 18:04:33 -0600

    Update JellyRoll.java

--- JellyRoll.java
+++ JellyRoll.java
@@ -1,10 +1,10 @@
 public class JellyRoll extends Roll {
     JellyRoll()
     {
+        //To set price of jelly roll, filling, sauce, and topping.
         super("Jelly Roll", 6.50);
         this.fillingPrice = 1.75;
         this.saucePrice = 2.00;
         this.toppingPrice = 1.50;
     }
-
 }
