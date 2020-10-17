74e2a21e111f3cc31c69d1a10ee0fd0d84094532
TongxinZHU <55003360+TongxinZHU@users.noreply.github.com>
2020-10-16 18:06:50 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,16 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        // test
+        //Test.
         JUnitTest test = new JUnitTest();
-
         test.test_customer_factory();
         test.testAdd();
         test.getNameTest();
 
-
-        // read input from user
+        //Ask users to input a value and read the value.
         System.out.println("How many days would you like the Roll Store Simulator to run?");
         Scanner in = new Scanner(System.in);
         int days = in.nextInt();
