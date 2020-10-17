2f3b752b1e82d2901cec130f3d09f553419e1b03
ruijiang-ma <55000634+ruijiang-ma@users.noreply.github.com>
2020-10-16 19:20:41 -0600

    Add files via upload

--- JUnitTest.java
+++ JUnitTest.java
@@ -3,25 +3,29 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class JUnitTest {
-    public void test_customer_factory() {
+
+    @Test
+    public void testCustomerFactory() {
         System.out.print("test CuastomerFactory...");
         assertTrue("failure - CustomerFactory.create(Customer[])", CustomerFactory.createCustomer() instanceof Customer[]);
         System.out.println("ok");
 
     }
 
+
     public void testAdd(){
-        SpringRoll springRoll = new SpringRoll();
-        EggRoll eggRoll = new EggRoll();
+        Roll springRoll = RollFactory.createRoll("Spring Roll");
+        Roll eggRoll = RollFactory.createRoll("Egg Roll");
         double price1 = springRoll.rollPrice;
         double price2 = eggRoll.rollPrice;
 
         double result = price1 + price2;
         assertTrue(result == 13.5);
-        System.out.println("testAdd() ok...");
+        System.out.println("testAdd()...ok");
     }
 
-    public void getNameTest () {
+    @Test
+    public void testGetName () {
 
         String message = "Sausage Roll";
         SausageRoll sausageRoll = new SausageRoll();
@@ -29,4 +33,5 @@ public class JUnitTest {
         System.out.println("Inside testgetName()...ok");
 
     }
+
 }
---
+++ RollFactory.java
@@ -0,0 +1,23 @@
+public class RollFactory implements Factory{
+    public static Roll createRoll(String type)
+    {
+        if (type == null || type.isEmpty())
+            return null;
+        if ("Egg Roll".equals(type)) {
+            return new EggRoll();
+        }
+        else if ("Spring Roll".equals(type)) {
+            return new SpringRoll();
+        }
+        else if ("Pastry Roll".equals(type)) {
+            return new PastryRoll();
+        }
+        else if ("Jelly Roll".equals(type)) {
+            return new JellyRoll();
+        }
+        else if ("Sausage Roll".equals(type)) {
+            return new SausageRoll();
+        }
+        return null;
+    }
+}
