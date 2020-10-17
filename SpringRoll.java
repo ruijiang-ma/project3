9fd7e2e1e978c452d5d2339fcfa2fad7939bee3d
TongxinZHU <55003360+TongxinZHU@users.noreply.github.com>
2020-10-16 18:09:51 -0600

    Update SpringRoll.java

--- SpringRoll.java
+++ SpringRoll.java
@@ -1,7 +1,6 @@
 public class SpringRoll extends Roll {
-
     SpringRoll() {
-
+        //To set price of spring roll, filling, sauce, and topping.
         super("Spring Roll", 5.00);
         this.fillingPrice = 1.00;
         this.saucePrice = 2.00;
