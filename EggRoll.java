f24be9c1d32aa4e9e81fa490c0aeba0997516f89
TongxinZHU <55003360+TongxinZHU@users.noreply.github.com>
2020-10-16 18:05:09 -0600

    Update EggRoll.java

--- EggRoll.java
+++ EggRoll.java
@@ -1,6 +1,7 @@
 public class EggRoll extends Roll {
     EggRoll()
     {
+        //To set price of egg roll, filling, sauce, and topping.
         super("Egg Roll", 8.50);
         this.fillingPrice = 5.50;
         this.saucePrice = 3.00;
